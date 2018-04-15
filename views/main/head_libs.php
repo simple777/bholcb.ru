167e249b86d78aaa9b0b98d39cab8f18520985cf
simple777 <613200@inbox.ru>
2018-04-15 21:29:49 +0300

    11-04-2018 - FIX BUGS
    add vk link, counter ya.metrica

--- views/main/head_libs.php
+++ views/main/head_libs.php
@@ -1,3 +1,3 @@
 <link rel="stylesheet" href="<?php echo SITEURL;?>css/normalize.min.css">
-    <link rel="stylesheet" href="<?php echo SITEURL;?>css/main.css?v19">
-    <link rel="stylesheet" href="<?php echo SITEURL;?>css/good-vision.css"><?php echo "\n";?>
+    <link rel="stylesheet" href="<?php echo SITEURL;?>css/main.css?v20">
+    <link rel="stylesheet" href="<?php echo SITEURL;?>css/good-vision.css?v10"><?php echo "\n";?>
